ee33104c8ade464049ad472abac0809e789b4994
Antoni <antekulmaniec0@gmail.com>
2025-06-03 21:33:24 +0200

    frontend start

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Witaj w React!</h1>
+      <p>To jest moja pierwsza, prosta aplikacja.</p>
+    </div>
+  );
+}
+
+export default App;
